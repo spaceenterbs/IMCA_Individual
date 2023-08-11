e6e4c8567d9b7a7a67c839fada0b039ca8bedae4
jinwoo <sds7629>
2023-08-11 16:29:23 +0900

    box 완

--- boxapi/operator.py
+++ boxapi/operator.py
@@ -5,15 +5,21 @@ from . import views
 def start():
     scheduler = BackgroundScheduler()
 
-    @scheduler.scheduled_job("cron", day_of_week="wed", hour="23", id="delete")
+    @scheduler.scheduled_job(
+        "cron", day_of_week="fri", hour="17", minute="30", id="delete"
+    )
     def auto_del_data():
         views.del_data()
 
-    @scheduler.scheduled_job("cron", day_of_week="wed", hour="23", id="musical")
+    @scheduler.scheduled_job(
+        "cron", day_of_week="fri", hour="17", minute="30", id="musical"
+    )
     def auto_get_musical():
         views.get_musical()
 
-    @scheduler.scheduled_job("cron", day_of_week="wed", hour="23", id="theater")
+    @scheduler.scheduled_job(
+        "cron", day_of_week="fri", hour="17", minute="30", id="theater"
+    )
     def auto_get_theater():
         views.get_theater()
 
