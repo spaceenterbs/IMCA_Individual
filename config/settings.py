e60b15b47fa4153e18353c347e5b8e33668c398d
jinwoo <sds7629>
2023-08-17 15:14:56 +0900

    token

--- config/settings.py
+++ config/settings.py
@@ -85,8 +85,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(day=20),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=20),
     "BLACKLIST_AFTER_ROTATION": False,
     "ALGORITHM": "HS256",
     "SIGNING_KEY": SECRET_KEY,
