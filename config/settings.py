7e4ab7e1d53336d215f3c8f7887b60b1ee4dd39c
enterspacebs <anotherhell48@gmail.com>
2023-08-23 19:24:14 +0900

    token

--- config/settings.py
+++ config/settings.py
@@ -81,7 +81,8 @@ REST_FRAMEWORK = {
     # "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+        # "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
