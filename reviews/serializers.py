f8e2b7844d09403c64db0b4dfdc455161afc0e7e
enterspacebs <anotherhell48@gmail.com>
2023-08-25 22:01:23 +0900

    review_writer SemiUserSerializer

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 from .models import Review
+from users.serializers import SemiUserSerializer
 
 
 class ReviewSerializer(ModelSerializer):
     writer_profile_img = serializers.URLField(
         source="review_author.profileImg", read_only=True
     )
+    review_writer = SemiUserSerializer(read_only=True)
 
     class Meta:
         model = Review
