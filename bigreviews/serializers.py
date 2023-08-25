6ea3e9f28054009ebbb5e0b13e2dccbcd055794a
enterspacebs <anotherhell48@gmail.com>
2023-08-25 16:46:39 +0900

    profileImg serializer added

--- bigreviews/serializers.py
+++ bigreviews/serializers.py
@@ -7,7 +7,9 @@ from .models import Bigreview
 class BigreviewSerializer(ModelSerializer):
     created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M")  # 년-월-일 시:분 형식으로 변환
     updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M")
-    # total_comments = serializers.SerializerMethodField()
+    writer_profile_img = serializers.URLField(
+        source="bigreview_writer.profileImg", read_only=True
+    )
 
     class Meta:
         model = Bigreview
--- reviews/serializers.py
+++ reviews/serializers.py
@@ -7,6 +7,9 @@ class ReviewSerializer(ModelSerializer):
     # total_comments = serializers.SerializerMethodField()
     created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M")  # 년-월-일 시:분 형식으로 변환
     updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M")
+    writer_profile_img = serializers.URLField(
+        source="review_author.profileImg", read_only=True
+    )
 
     class Meta:
         model = Review
