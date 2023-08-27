b9f439684a317ff289f5bfa47a2ee18fd6c3550e
spaceenterbs <anotherhell48@gmail.com>
2023-08-27 14:35:02 +0900

    config reroll

--- config/urls.py
+++ config/urls.py
@@ -38,8 +38,7 @@ urlpatterns = [
     path("boxoffice/", include("boxapi.urls")),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += [
